9a2675f4bd6642271a7e0aa385ae0df9f96e086d
nduran <n.duran@emakina.com.tr>
2019-08-22 13:58:15 +0300

    configured as out of box storefront

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://localhost:9002',
+          prefix: '/rest/v2/',
+          legacy: false
+        }
+      },
+      authentication: {
+        client_id: 'mobile_android',
+        client_secret: 'secret'
+      },
+      context: {
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
